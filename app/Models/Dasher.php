7d647c4bf9e4ff1299183da9b5c295309c9c3207
ralphpetallo <ralphpetallo@gmail.com>
2026-03-16 19:30:03 +0800

    added role

--- app/Models/Dasher.php
+++ app/Models/Dasher.php
@@ -17,6 +17,7 @@ class Dasher extends Authenticatable
         'first_name',
         'last_name',
         'email',
+        'role',
         'profile_photo',
         'password',
         'active_status',
