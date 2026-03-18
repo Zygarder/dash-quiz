ef2b108bf7c3f190ed017d6bade2ec7cdc78e9b6
ralphpetallo <ralphpetallo@gmail.com>
2026-03-18 14:08:09 +0700

    changes the baseLocal, getting csrf err because it should be align to webpage URL

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import router from "./router"
 
 // Global defaults
-axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = "http://127.0.0.1:8000"
 axios.defaults.withCredentials = true
 axios.defaults.withXSRFToken = true
 
