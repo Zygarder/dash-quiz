44b8ebf4ebdacb06d16d52dbb230afbbea06a99f
ralphpetallo <ralphpetallo@gmail.com>
2026-04-22 22:15:13 +0800

    tab icon set

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taft CSS Assessment Portal</title>
+    <link rel="icon" href="{{ asset('bolt.png') }}" type="image/x-icon">
     @vite(['resources/js/app.js'])
 </head>
 
