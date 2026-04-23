a5da8ad34330de4ca4bac87f65d263393974940b
ralphpetallo <ralphpetallo@gmail.com>
2026-04-23 21:06:34 +0800

    change logo to main logo

--- resources/js/components/AdminSide/AdminSideBar.vue
+++ resources/js/components/AdminSide/AdminSideBar.vue
@@ -1,13 +1,7 @@
 <template>
   <aside class="admin-sidebar" :class="{ 'open': isSidebarOpen }">
     <div class="logo-section">
-      <div class="logo-mark">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2">
-          <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-          <line x1="3" y1="9" x2="21" y2="9" />
-          <line x1="9" y1="21" x2="9" y2="9" />
-        </svg>
-      </div>
+        <img src="/public/bolt.png" alt="Logo" width="32" height="32" >
       <span class="logo-text">DASH<span>QUIZ</span></span>
     </div>
 
