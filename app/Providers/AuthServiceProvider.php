d29082ea5d235e4e68f4d77856b918a5d6958f09
ralphpetallo <ralphpetallo@gmail.com>
2026-04-25 09:43:34 +0800

    custom url for reseting password

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,25 +4,21 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         $this->registerPolicies();
 
-        //
+        // create custom url for reset password for dashers
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return config('app.url') . "/reset?token={$token}&email={$user->email}";
+        });
     }
 }
